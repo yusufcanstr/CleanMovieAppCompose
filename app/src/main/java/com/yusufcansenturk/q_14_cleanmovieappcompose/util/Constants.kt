37c96ebce18bd536151b494159912438167155f6
Yusufcan Senturk <yusufcansenturk10@gmail.com>
2023-06-15 09:53:55 +0300

    API Descripton

---
+++ app/src/main/java/com/yusufcansenturk/q_14_cleanmovieappcompose/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.yusufcansenturk.q_14_cleanmovieappcompose.util
+
+object Constants {
+    //http://www.omdbapi.com/?apikey=f30decb3&i=tt0372784
+    const val API_KEY = "f30decb3"
+    const val BASE_URL = "http://www.omdbapi.com"
+    const val IMDB_ID = "imdb_id"
+}
