24bcdba1f1de8c4478697e597e1ce4f40e27653e
Yusufcan Senturk <yusufcansenturk10@gmail.com>
2023-06-16 16:51:04 +0300

    Search Bar

--- app/src/main/java/com/yusufcansenturk/q_14_cleanmovieappcompose/presentation/movies/MoviesViewModel.kt
+++ app/src/main/java/com/yusufcansenturk/q_14_cleanmovieappcompose/presentation/movies/MoviesViewModel.kt
@@ -14,22 +14,22 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MoviesViewModel @Inject constructor(
-    private val getMovieUseCase: GetMovieUseCase
+    private val getMovieUseCase: GetMovieUseCase,
 ) : ViewModel() {
 
     private val _state = mutableStateOf<MoviesState>(MoviesState())
-    val state : State<MoviesState> = _state
+    val state: State<MoviesState> = _state
 
-    private var job : Job? = null
+    private var job: Job? = null
 
     init {
         getMovies(_state.value.search)
     }
 
-    private fun getMovies(search : String) {
+    private fun getMovies(search: String) {
         job?.cancel()
         job = getMovieUseCase.executeGetMovies(search).onEach {
-            when(it) {
+            when (it) {
                 is Resource.Success -> {
                     _state.value = MoviesState(movies = it.data ?: emptyList())
                 }
@@ -45,8 +45,8 @@ class MoviesViewModel @Inject constructor(
         }.launchIn(viewModelScope)
     }
 
-    fun onEvent(event : MoviesEvent) {
-        when(event) {
+    fun onEvent(event: MoviesEvent) {
+        when (event) {
             is MoviesEvent.Search -> {
                 getMovies(event.searchString)
             }
--- app/src/main/java/com/yusufcansenturk/q_14_cleanmovieappcompose/presentation/movies/views/MovieScreen.kt
+++ app/src/main/java/com/yusufcansenturk/q_14_cleanmovieappcompose/presentation/movies/views/MovieScreen.kt
@@ -1,36 +1,117 @@
 package com.yusufcansenturk.q_14_cleanmovieappcompose.presentation.movies.views
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
+import com.yusufcansenturk.q_14_cleanmovieappcompose.presentation.movies.MoviesEvent
 import com.yusufcansenturk.q_14_cleanmovieappcompose.presentation.movies.MoviesViewModel
 
 
 @Composable
 fun MovieScreen(
     navController: NavController,
-    viewModel: MoviesViewModel = hiltViewModel()
+    viewModel: MoviesViewModel = hiltViewModel(),
 ) {
 
     val state = viewModel.state.value
 
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.Black)) {
-        Column() {
-            
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+        Column {
+            MovieSearchBar(modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+                hint = "Batman",
+                onSearch = {
+                    viewModel.onEvent(MoviesEvent.Search(it))
+                }
+            )
+
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(state.movies) { movie ->
+                    Text(
+                        text = movie.Title,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        color = Color.White
+                    )
+                    println("FİLİM BAŞLIKLARI")
+                    println("TAG -> "  + movie.Title)
+                }
+            }
         }
     }
+}
 
+@Composable
+fun MovieSearchBar(
+    modifier: Modifier,
+    hint: String = "",
+    onSearch: (String) -> Unit = {},
+) {
 
+    var text by remember {
+        mutableStateOf("")
+    }
+
+    var isHintDisplayed by remember {
+        mutableStateOf(hint != "")
+    }
+
+    Box(modifier = modifier) {
+        TextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            keyboardActions = KeyboardActions(onDone = {
+                onSearch(text)
+            }),
+            maxLines = 1,
+            singleLine = true,
+            textStyle = TextStyle(color = Color.Black),
+            shape = RoundedCornerShape(10.dp),
+            colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.White),
+
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(5.dp, CircleShape)
+                .padding(20.dp)
+                .onFocusChanged {
+                    isHintDisplayed = it.isFocused != true && text.isEmpty()
+                }
+        )
+
+        if (isHintDisplayed) {
+            Text(
+                text = hint,
+                color = Color.LightGray,
+                modifier = Modifier.padding(horizontal = 20.dp, vertical = 12.dp)
+            )
+        }
+    }
 }
 
 
